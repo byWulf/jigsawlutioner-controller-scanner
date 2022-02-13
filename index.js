65116336da6e1e78f9b5fbfd00a82676874938a7
Michael Wolf <michael.wolf@check24.de>
2022-02-13 18:39:12 +0100

    Switch to libcamera-apps

--- index.js
+++ index.js
@@ -1,34 +1,10 @@
 import Controller from 'jigsawlutioner-controller';
-import camera from './camera.js';
-import sharp from 'sharp';
+import camera from './libcamera.js';
 
 const controller = new Controller(3000);
 
 controller.createEndpoint('take-photo', async (parameters, resolve) => {
-    const photo = await camera.takeImage();
+    const photo = await camera.takeImage(parameters.left || null, parameters.right || null, parameters.top || null, parameters.bottom || null, parameters.width || null);
 
-    const image = sharp(photo);
-
-    if (parameters.rotation) {
-        image.rotate(parseInt(parameters.rotation, 10));
-    }
-
-    const metadata = await image.metadata();
-
-    let left = Math.floor((parameters.left || 0) / 100 * metadata.width);
-    let top = Math.floor((parameters.top || 0) / 100 * metadata.height);
-    let width = Math.floor(((parameters.right || 100) - (parameters.left || 0)) / 100 * metadata.width);
-    let height = Math.floor(((parameters.bottom || 100) - (parameters.top || 0)) / 100 * metadata.height);
-
-    const resizedPhoto = await image.extract({
-        left: left,
-        top: top,
-        width: width,
-        height: height
-    }).resize(
-        Math.floor((width / height) * (parseInt(parameters.width, 10) || metadata.width)),
-        (parseInt(parameters.width, 10) || metadata.width)
-    ).toBuffer();
-
-    resolve(resizedPhoto, 'image/jpeg');
+    resolve(photo, 'image/jpeg');
 });
---
+++ libcamera.js
@@ -0,0 +1,135 @@
+import childProcess from 'child_process';
+
+export default new class Libcamera {
+    isInitialized = false;
+    currentCameraResolver = null;
+    cameraProcess = null;
+    buffer = null;
+    startTime = 0;
+
+    currentParameters = null;
+
+    parameters = [
+        '-t', '0', // Time (in ms) for which program runs
+        '-s', // Perform capture when signal received
+        '--shutter', '20000', // Set a fixed shutter speed
+        '-o', '-', // Set the output file name
+        '--gain', '0', // Set a fixed gain value
+        '--sharpness', '1',
+        '--contrast', '1',
+        '--brightness', '0',
+        '--saturation', '0.5',
+        '--awb', 'fluorescent',
+        '--denoise', 'off',
+        '--thumb', 'none',
+        '-q', '50', // Jpg quality
+        '-n', // Do not show a preview window
+        '-e', 'jpg', // Encoding
+    ];
+
+    /**
+     * @return {Promise<void>}
+     */
+    init(left, right, top, bottom, width) {
+        return new Promise((resolve) => {
+            if (this.isInitialized) {
+                resolve();
+                return;
+            }
+
+            const parameters = [...this.parameters];
+            let resizeFactor = 0.75;
+            if (left || right || top || bottom) {
+                parameters.push('--roi');
+                parameters.push(left + ',' + top + ',' + (right - left) + ',' + (bottom - top))
+                resizeFactor = 0.75 * ((bottom - top) / (right - left));
+            }
+            if (width) {
+                parameters.push('--width', width);
+                let height = Math.round(width * resizeFactor);
+                parameters.push('--height', height + (height % 2));
+            }
+
+            this.cameraProcess = childProcess.spawn('libcamera-still', parameters);
+
+            this.cameraProcess.stdout.on('data', (data) => {
+                this.handleImageData(data);
+            });
+
+            this.cameraProcess.stderr.on('data', (data) => {
+                console.error(data.toString());
+            });
+            this.cameraProcess.on('close', () => {
+                console.log('Camera closed!');
+            });
+
+            setTimeout(() => {
+                this.isInitialized = true;
+                resolve();
+            }, 2000);
+        });
+    };
+
+    /**
+     * @param {Buffer} data
+     */
+    handleImageData(data) {
+        if (this.buffer === null) {
+            this.buffer = data;
+        } else {
+            this.buffer = Buffer.concat([this.buffer, data]);
+        }
+
+        let openings = 1;
+        let currentPosition = 2;
+        do {
+            let nextStart = this.buffer.indexOf(Buffer.from([0xff, 0xd8]), currentPosition);
+            let nextEnd = this.buffer.indexOf(Buffer.from([0xff, 0xd9]), currentPosition);
+
+            if (nextEnd === -1) {
+                return;
+            }
+            if (nextStart === -1 || nextEnd < nextStart) {
+                currentPosition = nextEnd + 2;
+                openings--;
+            } else {
+                currentPosition = nextStart + 2;
+                openings++;
+            }
+        } while (openings > 0);
+
+
+        let finishedImageBuffer = this.buffer.slice(0, currentPosition);
+        this.buffer = this.buffer.slice(currentPosition);
+
+        if (this.currentCameraResolver !== null) {
+            console.log('Taking image took ' + (Date.now() - this.startTime) + 'ms (buffer length ' + this.buffer.length + ' remaining)');
+
+            this.currentCameraResolver(finishedImageBuffer);
+        }
+
+        // Look if there is another image in the same stream already
+        this.handleImageData(Buffer.from(''));
+    };
+
+    /**
+     * @return {Promise<Buffer>}
+     */
+    takeImage(left, right, top, bottom, width) {
+        return new Promise(async (resolve) => {
+            if (this.isInitialized && this.currentParameters !== left + '_' + right + '_' + top + '_' + bottom + '_' + width) {
+                this.cameraProcess.kill('SIGINT');
+                this.cameraProcess = null;
+                this.isInitialized = false;
+            }
+            if (!this.isInitialized) {
+                await this.init(left, right, top, bottom, width);
+                this.currentParameters = left + '_' + right + '_' + top + '_' + bottom + '_' + width;
+            }
+
+            this.startTime = Date.now();
+            this.currentCameraResolver = resolve;
+            this.cameraProcess.kill('SIGUSR1');
+        });
+    };
+}
