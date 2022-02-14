76b7c60acaba56aae1e785b11e192dcab33ae5f7
Michael Wolf <michael.wolf@check24.de>
2022-02-14 21:07:43 +0100

    Add light pin to toggle bottom and top light

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const controller = new Controller(3000);
 
 controller.createEndpoint('take-photo', async (parameters, resolve) => {
     const pin = await controller.getOutputPin(parameters, 'light');
-    pin.writeSync(parameters.topLight === 'on' ? 0 : 1);
+    if (['top', 'bottom'].indexOf(parameters.light.position) === -1) {
+        throw new Error('Parameter "light[position]" was not one of "top", "bottom".');
+    }
+    pin.writeSync(parameters.light.position === 'top' ? 0 : 1);
 
     const photo = await camera.takeImage(parameters.left || null, parameters.right || null, parameters.top || null, parameters.bottom || null, parameters.width || null);
 
